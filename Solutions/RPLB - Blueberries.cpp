91a589e49dbb1afcab0e55d721cba375fc2c9994
Anik chakraborty <anikchakraborty562@gmail.com>
2022-07-16 22:55:13 +0600

    Create RPLB - Blueberries.cpp

---
+++ Solutions/RPLB - Blueberries.cpp
@@ -0,0 +1,147 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include<bits/stdc++.h>
+#include<time.h>
+#include <iomanip>
+ 
+using    namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+#define          ll                     long long int
+#define          EPS                    10E-10
+#define          ull                    unsigned long long int
+#define          db                     double
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          mxx                    100005
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          ff                     first
+#define          ss                     second
+#define          re                     return
+#define          QI                     queue<int>
+#define          SI                     stack<int>
+#define          iseq(a,b)              (fabs(a-b)<eps)
+#define          SZ(x)                  ((int) (x).size())
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          scch1(ch)              scanf("%s",ch)
+#define          scch2(ch1,ch2)         scanf("%s %s",ch1,ch2)
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     3.141592653589793238462643
+#define          rep( i , a , b )       for( i=a ; i<b ; i++)
+#define          rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define          repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define          RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          get_pos(c,x)           (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          CASEL(t)               printf("Case %d:\n",t)
+#define          Unique(X)             (X).erase(unique(all(X)),(X).end())
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+void FLASH() {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+void SETF() {cout.ios_base::setf(ios_base::fixed); cerr.ios_base::setf(ios_base::fixed);}
+void UNSETF() {cout.ios_base::unsetf(ios_base::fixed); cerr.ios_base::unsetf(ios_base::fixed);}
+//ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+//string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+//cout<<*X.find_by_order(0)<<endl;
+//cout<<X.order_of_key(-5)<<endl;
+ 
+bool sortinrev(const pair<ll,ll> &a,const pair<ll,ll> &b)
+{
+    return (a.first > b.first);
+}
+ 
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+ 
+///-----------------------Bitmask------------------
+///int Set(int N,int pos){return N=N | (1<<pos);}
+///int reset(int N,int pos){return N= N & ~(1<<pos);}
+///bool check(int N,int pos){return (bool)(N & (1<<pos));}
+///------------------------------------------------
+ 
+int wi[1005];
+int dp[1002][1002];
+void knapsack(int n,int W)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=W;j++)
+        {
+            if(i==1)
+            {
+                if(wi[i-1]<=j)dp[i][j]=max(dp[i-1][j-wi[i-1]]+wi[i-1],dp[i-1][j]);
+                else dp[i][j]=dp[i-1][j];
+            }
+            else if(wi[i-1]<=j)
+            {
+                dp[i][j]=max(dp[i-2][j-wi[i-1]]+wi[i-1],dp[i-1][j]);
+            }
+            else dp[i][j]=dp[i-1][j];
+        }
+    }
+}
+ 
+int main()
+{
+     FLASH();
+#ifndef ONLINE_JUDGE
+     time_t time_t1, time_t2;
+     time_t1 = clock();
+#endif
+     //Start
+     int t,tst=1;
+     cin>>t;
+     while(t--)
+     {
+        int n,W;
+        cin>>n>>W;
+        for(ll i=0; i<n; i++)cin>>wi[i];
+        knapsack(n,W);
+        cout<<"Scenario #"<<tst++<<": "<<dp[n][W]<<endl;
+        zero(wi);zero(dp);
+     }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+#ifndef ONLINE_JUDGE
+	time_t2 = clock();
+	SETF();
+	cerr<<"Time taken: "<<setprecision(7)<<(time_t2 - time_t1)/(double)CLOCKS_PER_SEC<<"\n";
+	UNSETF();
+#endif
+ 
+ 
+ 
+    return 0;
+}
+ 
