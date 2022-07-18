f5cee3485e9ab061450af13e27d496a87e0a9b2a
Anik chakraborty <anikchakraborty562@gmail.com>
2022-07-18 13:26:38 +0600

    Create TDPRIMES.cpp

---
+++ Solutions/TDPRIMES.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define          CIN                    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define          ll                     long long int
+#define          ull                    unsigned long long int
+#define          db                     double
+#define          pii                    pair < int, int>
+#define          pll                    pair < ll, ll>
+#define          MOD                    1000000007
+#define          vi                     vector<int>
+#define          vl                     vector<ll>
+#define          pb                     push_back
+#define          sc                     scanf
+#define          pf                     printf
+#define          scin(x)                scanf("%d",&(x))
+#define          scin2(x,y)             scanf("%d %d",&(x),&(y))
+#define          scln(x)                scanf("%lld",&(x))
+#define          scln2(x,y)             scanf("%lld %lld",&(x),&(y))
+#define          min3(a,b,c)            min(a,min(b,c))
+#define          min4(a,b,c,d)          min(d,min(a,min(b,c)))
+#define          max3(a,b,c)            max(a,max(b,c))
+#define          max4(a,b,c,d)          max(d,max(a,max(b,c)))
+#define          ms(a,b)                memset(a,b,sizeof(a))
+#define          mp                     make_pair
+#define          gcd(a, b)              __gcd(a,b)
+#define          lcm(a, b)              ((a)*(b)/gcd(a,b))
+#define          input                  freopen("input.txt","rt", stdin)
+#define          output                 freopen("output.txt","wt", stdout)
+#define          PI                     3.141592653589793238462643
+#define          rep( i , a , b )       for( i=a ; i<b ; i++)
+#define          rev( i , a , b )       for( i=a ; i>=b ; i--)
+#define          repx( i ,a,b, x)       for( i=a ; i<b ; i+=x)
+#define          RUN_CASE(t,T)          for(__typeof(t) t=1;t<=T;t++)
+#define          zero(a)                memset(a,0,sizeof a)
+#define          all(v)                 v.begin(),v.end()
+#define          get_pos(c,x)           (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define          CASEL(t)               printf("Case %d:\n",t)
+#define          Unique(X)             (X).erase(unique(all(X)),(X).end())
+
+///priority_queue<int,vector<int>,greater<int> >pq;
+///string str = "abcdefghijklmnopqrstuvwxyz";
+///string s2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+
+///---------------Order set-------------------
+///template<typename T> using orderset =tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+///#include <ext/pb_ds/assoc_container.hpp>
+///using namespace __gnu_pbds;
+///*os.find_by_order(index) os.order_of_key(value)
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+///sort( all( v ) ) ;
+long long int arr[100000000];
+long long int arr2[100000000];
+long long int prime()
+{
+   long long  int i,j;
+    arr[0]=2;
+    for(i=3; i<=sqrt(100000000); i=i+2)
+    {
+        if(arr2[i]==0)
+        {
+            for(j=i*i; j<=100000000; j=j+i+i)
+            {
+                arr2[j]=1;
+            }
+        }
+    }
+    j=1;
+    for(i=3; i<=100000000; i=i+2)
+    {
+        if(arr2[i]==0)
+        {
+            arr[j]=i;
+            j++;
+        }
+    }
+    return (j);
+}
+
+int main()
+{
+    long long int k,i;
+    k=prime();
+    for(i=0;i<k;i=i+100){
+        printf("%lld\n",arr[i]);
+    }
+
+    return 0;
+}
